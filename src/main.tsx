0ca0824acb8bc8b1708a26481f596d12a2ac0f16
twjw <aaaawjwa@gmail.com>
2024-03-13 10:56:44 +0800

    ReactDom.createRoot 改成 createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import '@/styles/eric-reset.css'
 import '@/styles/common.css'
 import 'virtual:uno.css'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import {App} from '@/app'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+createRoot(document.getElementById('root')!).render(<App />)
