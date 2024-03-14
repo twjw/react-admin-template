8a827c42cf9f3054ff9e97ade30bbd24a3f5d2d6
twjw <aaaawjwa@gmail.com>
2024-03-14 15:24:27 +0800

    build: 添加 wtbx-type-safe-fetch package

--- mock-apis/user.js
+++ mock-apis/user.js
@@ -1,11 +1,15 @@
 export default {
 	// expiredMinutes number 過期分鐘數
-	'/api/user/login': ({ body, query }) => {
-		if (body.username !== 'test' && body.password !== '123123')
+	'/api/user/login': ({ res, body, query }) => {
+		res.setHeader('Content-Type', 'application/json; charset=utf-8')
+
+		if (body.username !== 'test' && body.password !== '123123') {
+			res.statusCode = 500
 			return {
 				code: 500,
 				data: '帳號或密碼錯誤',
 			}
+		}
 
 		return {
 			code: 200,
@@ -14,19 +18,24 @@ export default {
 			},
 		}
 	},
-	'/api/user/profile': ({ headers }) => {
+	'/api/user/profile': ({ res, headers }) => {
+		res.setHeader('Content-Type', 'application/json; charset=utf-8')
+
 		if (!headers.Authorization || !headers.Authorization?.test(/^Bearer\s\d+/)) {
+			res.statusCode = 403
 			return {
 				code: 403,
 				data: '請傳遞有效的驗證參數',
 			}
 		}
 
-		if (Date.now() > Number(headers.Authorization.match(/^Bearer\s(\d+)/)[1]))
+		if (Date.now() > Number(headers.Authorization.match(/^Bearer\s(\d+)/)[1])) {
+			res.statusCode = 403
 			return {
 				code: 403,
 				data: 'token 已失效',
 			}
+		}
 
 		return {
 			code: 200,
